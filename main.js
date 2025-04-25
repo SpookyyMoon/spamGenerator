1b6722c78f6cbb27afd44a3b3e707c0c853db167
Jacobo <spookyluna666@gmail.com>
2025-04-25 17:30:50 +0200

    Envio de correos funcional

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const spamgen = require('./modulos/spamgen.js');
 const spamgenInstancia = new spamgen;
 // Prompt
 const prompt = require('prompt-sync')();
-
 // Funcion menu
 function menu(){
     console.clear();
@@ -13,28 +12,79 @@ function menu(){
 
     === Spam Generator ===
 
-        1. Enviar correo
+        1. Enviar correos
         2. Añadir correos
-        3. Añadir desde .xlsx
-        4. Añadir desde .csv
+        3. Configurar spam
+        5. Mostrar lista de spam
 
-        5. Salir
+        6. Salir
         
     `);
 
     let opcion = Number(prompt ("Selecciona una opción: "));
     switch (opcion){
         case 1:
+            spamgenInstancia.enviarSpam();
             break;
         case 2:
+            console.clear();
+            //Muestra opciones
+            console.log(`
+
+    === Spam Generator ===
+            
+        1. Añadir desde terminal
+        2. Importar desde .xlsx
+        3. Importar desde .csv
+            
+        4. Volver`);
+
+            let opcionNested = Number(prompt ("Selecciona una opción: "))
+            
+            switch(opcionNested){
+                case 1:
+                    addCorreos();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    return menu();
+            }
             break;
         case 3:
+            console.clear();
+            //Muestra opciones
+            console.log(`
+
+    === Spam Generator ===
+            
+        1. Configurar remitente
+        2. Modificar asunto
+        3. Modificar mensaje
+            
+        4. Volver`);
+
+            let opcionNested2 = Number(prompt ("Selecciona una opción: "))
+            
+            switch(opcionNested2){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    return menu();
+            }
             break;
         case 4:
             break;
         case 5:
+            mostrarLista();
             break;
-        case 5:
+        case 6:
             process.exit();
         default:
             console.log("Has seleccionado una opción inválida!");
@@ -53,24 +103,30 @@ function addCorreos(){
     Instrucciones:
 
     Introduce uno a uno los correos que quieras añadir, introduce "salir" para dejar
-    de introducir correos. El correo debe seguir el siguiente formato [correo@proveedor.dominio]`);
+    de introducir correos. El correo debe seguir el siguiente formato [correo@proveedor.dominio]\n`);
     
-    let opcion = prompt ("\n¿Quieres añadir nuevos correos? [S/N]: ");
+    let opcion = prompt ("¿Quieres añadir nuevos correos? [S/N]: ");
     if(opcion == "S" || opcion == "s"){
-        let correo;
-        while(correo != "salir" || correo != "Salir"){
-            correo = prompt ("\nIntroduce un correo para añadir a la lista de spam: ");
+        let correo = null;
+        while(true){
+            console.clear();
+            console.log(" === Spam Generator === ")
+            console.log("  == Añadir correos ==\n");
+            correo = prompt ("Introduce un correo para añadir a la lista de spam: ");
+            console.log(correo);
             if(correo.includes('@')){
                 spamgenInstancia.correoAdd(correo);
                 console.log(`\nCorreo "${correo} añadido correctamente!`);
+            }
+            else if(correo == "Salir" || correo == "salir"){
+                console.log("Volviendo...")
+                await();
+                return menu();
             }
             else{
                 console.log("\nHas introducido un correo inválido! Recuerda seguir el formato especificado!");
             }
         }
-        console.log("Volviendo...")
-        await();
-        return menu();
     }
     else{
         console.log("Volviendo...")
@@ -79,8 +135,29 @@ function addCorreos(){
     }
 }
 
-function await(){
-    return new Promise(resolve => setTimeout(resolve, 2));
+function mostrarLista(){
+    console.clear();
+    console.log(" === Spam Generator === ");
+    console.log("  == Añadir correos ==\n");
+
+    let opcion = prompt("¿Quieres ver la lista actual de spam? [S/N]: ")
+    if (opcion == "S" || opcion == "s"){
+        spamgenInstancia.listaMostrar();
+        prompt("Pulsa enter para volver...");
+            return menu();
+    }
+    else{
+        console.log("Volviendo...")
+        await();
+        return menu();
+    }
+
 }
+
+function await(){
+    return new Promise(resolve => setTimeout(resolve, 3));
+}
+
+menu();
 
 module.exports = menu;
--- modulos/spamgen.js
+++ modulos/spamgen.js
@@ -1,12 +1,50 @@
-//Array temporal almacena correos
-let correosLista = ['wiwelav972@astimei.com'];
+//Import nodemailer
+const nodemailer = require("nodemailer");
 
+//Configuracion de nodemailer usando mailtrap
+var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "401d0fae5fe213",
+      pass: "b4c1777447ac9f"
+    }
+});
+//Configuracion de correo
+var mailOptions = {
+    from: 'janedoe@gmail.com',
+    to: 'johndoe@gmail.com',
+    subject: 'Asunto',
+    text: 'Mensaje'
+};
+
+//Array temporal almacena correos
+let spamLista = ['wiwelav972@astimei.com'];
+
+//Clase y métodos
 class SpamGen{
 
     correoAdd(correo){
-        correosLista.push(correo);
+        spamLista.push(correo);
     }    
 
+    listaMostrar(){
+        console.log("Lista actual de spam: \n")
+        spamLista.forEach(correo => {
+            console.log(" " + correo);
+        });
+    }
+
+    //Envio de correo
+    enviarSpam(){
+        transport.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Spam enviado con éxito!");
+            }
+        });
+    }
 }
 
 module.exports = SpamGen;
